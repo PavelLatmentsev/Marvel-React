adfd78a5b5786ec1f19c0bc56bc20ac397b26453
Pavel <nightcharm@mail.ru>
2023-05-09 14:08:23 +0300

    add lazy and dinamyc import

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,20 +1,27 @@
+import { lazy,Suspense } from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import AppHeader from "../appHeader/AppHeader";
-import { MainPage, ComicsPage, Page404, SingleComicPage } from '../pages';
-
+// import { MainPage, ComicsPage, SingleComicPage} from '../pages';
+// import Page404 from '../pages/404';
+import Spinner from '../spinner/spinner';
+const Page404= lazy(() => import("../pages/404"))
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
 const App = () => {
-    
     return (
         <Router>
             <div className="app">
                 <AppHeader/>
                 <main>
+                <Suspense fallback={<Spinner/>}>
                     <Routes>
                         <Route  path="/" element={<MainPage/>}/>               
                         <Route  path="/comics" element={<ComicsPage/>}/>
                         <Route  path="*" element={<Page404 />}/>
                         <Route  path="comics/:comicId" element={<SingleComicPage />} errorElement={<Page404 />}/>
                     </Routes>
+                    </Suspense>
                 </main>
             </div>
         </Router>
