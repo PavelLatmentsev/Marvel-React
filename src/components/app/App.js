93c0f3d9f48536b08579548dfb977fafa7569fdb
Pavel <nightcharm@mail.ru>
2023-05-09 12:15:27 +0300

    add single comics page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,6 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import AppHeader from "../appHeader/AppHeader";
-import { MainPage, ComicsPage } from '../pages';
+import { MainPage, ComicsPage, Page404, SingleComicPage } from '../pages';
 
 const App = () => {
     
@@ -12,7 +12,8 @@ const App = () => {
                     <Routes>
                         <Route  path="/" element={<MainPage/>}/>               
                         <Route  path="/comics" element={<ComicsPage/>}/>
-                        
+                        <Route  path="*" element={<Page404 />}/>
+                        <Route  path="comics/:comicId" element={<SingleComicPage />} errorElement={<Page404 />}/>
                     </Routes>
                 </main>
             </div>
